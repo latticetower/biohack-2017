12710e3d6556c227948bf572f484589ddb6bb88b
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2017-03-04 17:53:22 +0300

    minor crucial corrections

--- chain_gene.py
+++ chain_gene.py
@@ -1,25 +1,20 @@
 import csv
 import requests
 import io
-#pdb_id = '3ny5' #'5fcg #3ny5
-#gene = 'BRAF'
 
-def chain_gene(padb_id, gene):
-	pattern1 = 'http://www.rcsb.org/pdb/rest/customReport.csv?pdbids='
-	pattern2 = '&customReportColumns=geneName&format=csv&service=wsfile'
+def gene_chains(pdb_id, gene):
+    pattern1 = 'http://www.rcsb.org/pdb/rest/customReport.csv?pdbids='
+    pattern2 = '&customReportColumns=geneName&format=csv&service=wsfile'
 
-	r = requests.get(pattern1+pdb_id+pattern2)
+    r = requests.get(pattern1+pdb_id+pattern2)
 
-	page = list(csv.reader(io.StringIO(r.text)))
-	#page = [['structureId', 'chainId', 'geneName'], ['3NY5', 'A', 'BRAF#BRAF1#RAFB1'], ['3NY5', 'B', 'BRAF#BRAF1#RAFB1'], ['3NY5', 'C', 'BRAF#BRAF1#RAFB1'], ['3NY5', 'D', 'BRAF#BRAF1#RAFB1']]
-	number_of_chains = len(page) - 1
-	chains_gene = [] 
-	for line in page:
-		line = str(line).split(', ')
-		if (line[2].find(gene)!=-1):
-			chains_gene += line[1][1]
-	dict = {}
-	dict['chains_gene'] = chains_gene
-	dict['number_of_chains'] = number_of_chains
-	return(dict)
-print(chain_gene(pdb_id, gene))
+    page = list(csv.reader(io.StringIO(r.text)))
+    number_of_chains = len(page) - 1
+    chains = []
+    print(page)
+    for line in page:
+        if gene in line[2].split("#"):
+            chains += line[1]
+    return chains
+
+
--- kegg.py
+++ kegg.py
@@ -1,29 +1,80 @@
 from bioservices import KEGG
+import os
+
+def kegg_find(*args):
+    if not hasattr(kegg_find,"cache"):
+        if os.path.isfile("kegg_find.cache"):
+            kegg_find.cache = pickle.load(open("kegg_find.cache","rb"))
+        else:
+            kegg_find.cache = {}
+
+    if args not in kegg_find.cache or kegg_find.cache[args] is None:
+        k = KEGG()
+        result = k.find(*args)
+        kegg_find.cache[args] = result
+        with open("kegg_find.cache~","wb") as f:
+            pickle.dump(kegg_find.cache, f)
+        os.rename("kegg_find.cache~", "kegg_find.cache")
+        return result
+    else:
+        return kegg_find.cache[args]
+
+def kegg_get(*args):
+    if not hasattr(kegg_get,"cache"):
+        if os.path.isfile("kegg_get.cache"):
+            kegg_get.cache = pickle.load(open("kegg_get.cache","rb"))
+        else:
+            kegg_get.cache = {}
+
+    if args not in kegg_get.cache or kegg_get.cache[args] is None:
+        k = KEGG()
+        result = k.get(*args)
+        kegg_get.cache[args] = result
+        with open("kegg_get.cache~","wb") as f:
+            pickle.dump(kegg_get.cache, f)
+        os.rename("kegg_get.cache~", "kegg_get.cache")
+        return result
+    else:
+        return kegg_get.cache[args]
 
 
-def list_of_genes(genes):
-    all = []
-    for gene in genes:
-        all.append(get_pdb_id_by_name_gene(gene))
-    return all
-
-
-def get_pdb_id_by_name_gene(name_gene):
+def get_pdb_id_by_name_gene(gene_name):
     k = KEGG()
-    id_gene = []
+    gene_ids = []
 
     # по названию гена получаем id
-    gen = k.find("hsa", name_gene)
-    for i in gen.split():
-        if "hsa" in i:
-            id_gene.append(i.split(':')[1])
+    gen = kegg_find("hsa", gene_name)
+    if gen in [400, 404]:
+        return [],[]
+    for line in gen.split("\n"):
+        if len(line)>0:
+            gene_ids.append(line.split("\t")[0])
 
     # по каждому полученному id гена получаем PDB_ID
-    pdb_id = []
-    for key, value in enumerate(id_gene):
-        e = k.get('hsa:{}'.format(id_gene[key]))
-        d = k.parse(e)
-        if "STRUCTURE" in d:
-            pdb_id += d["STRUCTURE"]["PDB"].split()
+    pdb_ids = []
+    if len(gene_ids)>100:
+        return [],[]
+    for gene_id in gene_ids:
 
-    return (name_gene, id_gene, pdb_id)
+        e = kegg_get(gene_id)
+        if e in [400, 404]:
+            continue
+        d = k.parse(e)
+
+        if "STRUCTURE" in d:
+            pdb_ids += d["STRUCTURE"]["PDB"].split()
+    return gene_ids, pdb_ids
+
+gene_name_to_gene_ids_pdb_ids = {}
+
+if __name__ == "__main__":
+    import pickle
+    genes = pickle.load(open("oncogen_genes.pickle","rb"))
+    print(genes)
+    for i, gene in enumerate(genes):
+        print(i, gene)
+        gene_ids, pdb_ids = get_pdb_id_by_name_gene(gene)
+        print("%2d / %2d "%(i, len(genes)), gene, "{%d}{%d}"%(len(gene_ids),len(pdb_ids)))
+        gene_name_to_gene_ids_pdb_ids[gene] = get_pdb_id_by_name_gene(gene)
+    with open("gene_name_to_gene_ids_pdb_ids.pickle","wb") as f:
+        pickle.dump(gene_name_to_gene_ids_pdb_ids,f)
